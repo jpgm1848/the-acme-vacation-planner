d8e68413f7936a159717df2f50cd64ac046d9ef0
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-03-12 15:36:17 -0400

    finished guided practice

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/api/users", async (req, res, next) => {
   try {
@@ -38,6 +39,37 @@ app.get("/api/vacations", async (req, res, next) => {
   } catch (ex) {
     next(ex);
   }
+});
+
+app.delete("/api/users/:userId/vacations/:id", async (req, res, next) => {
+  try {
+    await destroyVacation({ id: req.params.id, user_id: req.params.userId });
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post("/api/users/:id/vacations", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    res
+      .status(201)
+      .send(
+        await createVacation({
+          user_id: req.params.id,
+          place_id: req.body.place_id,
+          travel_date: req.body.travel_date,
+        })
+      );
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message || err });
 });
 
 const init = async () => {
@@ -89,6 +121,12 @@ const init = async () => {
     console.log(`curl localhost:${port}/api/users`);
     console.log(`curl localhost:${port}/api/places`);
     console.log(`curl localhost:${port}/api/vacations`);
+    console.log(
+      `curl -X DELETE localhost:${port}/api/users/${moe.id}/vacations/${vacations[1].id}`
+    );
+    console.log(
+      `curl -X POST localhost:${port}/api/users/${ethyl.id}/vacations -d '{"travel_date": "04/01/2025", "place_id": "${seoul.id}"}' -H "Content-Type:application/json"`
+    );
   });
 };
 
